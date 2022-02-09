47e01bc3639bd7404c4934f9fa2a23776e7593c3
Fernando Segregio <segregio@gmail.com>
2022-02-09 17:55:25 -0300

    feat: Cria componente Header, limpa App padrao,  renderiza componente Header no App

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <h3>Sobre</h3>
+        <h3>Projetos</h3>
+        <h3>Skills</h3>
+        <h3>Contado</h3>
+      </nav>
+    </header>
+  );
+}
