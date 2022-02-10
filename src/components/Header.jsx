81773d9d5a2e55a742920f7102ea25a15b725b40
Fernando Segregio <segregio@gmail.com>
2022-02-10 00:29:35 -0300

    style + feat: adiciona estilos em linha para alinhar header, adiciona nome no header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { IoMdMoon as Moon } from 'react-icons/io';
+import { IoMdMoon as Moon, IoIosArrowBack as ArrowBack, IoIosArrowForward as ArrowForward } from 'react-icons/io';
 import { FiSun as Sun } from 'react-icons/fi';
 import brazil from '../images/brazil-circle.svg';
 import EUA from '../images/EUA-circle.svg';
@@ -30,48 +30,37 @@ export default function Header() {
   return (
     <header style={theme ? {
       display: 'flex',
+      justifyContent: 'space-around',
+      alignItems: 'center',
       flexWrap: 'nowrap',
       flexDirection: 'row',
       backgroundColor: 'black',
       color: 'white',
+      fontSize: '10px',
     } : {
       display: 'flex',
+      justifyContent: 'space-around',
+      alignItems: 'center',
       flexWrap: 'nowrap',
       flexDirection: 'row',
       backgroundColor: 'white',
       color: 'black',
+      fontSize: '10px',
     }}
     >
-      <nav style={{ display: 'flex' }}>
+      <nav style={{ display: 'flex', width: '100%', justifyContent: 'space-evenly' }}>
+        <div>
+          <h3 style={{ display: 'flex', alignItems: 'flex-end' }}>
+            <ArrowBack />
+            NandoDev/
+            <ArrowForward />
+          </h3>
+        </div>
         <h3>{ language === 'pt-BR' ? languageObject.titleNav1.pt : languageObject.titleNav1.en }</h3>
         <h3>{ language === 'pt-BR' ? languageObject.titleNav2.pt : languageObject.titleNav2.en }</h3>
         <h3>{ language === 'pt-BR' ? languageObject.titleNav3.pt : languageObject.titleNav3.en }</h3>
         <h3>{ language === 'pt-BR' ? languageObject.titleNav4.pt : languageObject.titleNav4.en }</h3>
       </nav>
-      <div>
-        <input
-          type="image"
-          value="pt-BR"
-          onClick={(e) => setLanguage(e.target.value)}
-          src={brazil}
-          alt={language === 'pt-BR'
-            ? languageObject.altFlagBrazil.pt
-            : languageObject.altFlagBrazil.en}
-          width="40px"
-          height="40px"
-        />
-        <input
-          type="image"
-          value="en-US"
-          onClick={(e) => setLanguage(e.target.value)}
-          src={EUA}
-          alt={language === 'pt-BR'
-            ? languageObject.altFlagEUA.pt
-            : languageObject.altFlagEUA.en}
-          width="40px"
-          height="40px"
-        />
-      </div>
       <div>
         <button
           type="button"
@@ -86,6 +75,36 @@ export default function Header() {
             ? <Sun style={{ color: 'white' }} className="theme-clean" />
             : <Moon className="theme-dark" />}
         </button>
+      </div>
+      <div style={{
+        display: 'flex',
+        width: '20%',
+        justifyContent: 'center',
+      }}
+      >
+        <input
+          type="image"
+          value="pt-BR"
+          onClick={(e) => setLanguage(e.target.value)}
+          src={brazil}
+          alt={language === 'pt-BR'
+            ? languageObject.altFlagBrazil.pt
+            : languageObject.altFlagBrazil.en}
+          width="20px"
+          height="20px"
+          style={{ marginRight: '5px' }}
+        />
+        <input
+          type="image"
+          value="en-US"
+          onClick={(e) => setLanguage(e.target.value)}
+          src={EUA}
+          alt={language === 'pt-BR'
+            ? languageObject.altFlagEUA.pt
+            : languageObject.altFlagEUA.en}
+          width="20px"
+          height="20px"
+        />
       </div>
     </header>
   );
