7737d6c8503c4b5f4f88d606a118558f8cc631aa
Fernando Segregio <segregio@gmail.com>
2022-02-10 23:47:46 -0300

    style + feat: adiciona logica do botao de tema, quebra o nome em 2 linhas.

--- src/components/SectionMain.jsx
+++ src/components/SectionMain.jsx
@@ -4,6 +4,7 @@ import PortfolioContext from '../context/PortfolioContext';
 export default function SectionMain() {
   const {
     language,
+    theme,
   } = useContext(PortfolioContext);
 
   const languageUser = () => language;
@@ -16,7 +17,8 @@ export default function SectionMain() {
 
   const languageObject = {
     greetingTitle: { pt: 'Olá, eu sou', en: 'Hi, I am' },
-    nameTitle: { pt: 'Fernando Segregio' },
+    firstNameTitle: { pt: 'Fernando' },
+    lastNameTitle: { pt: 'Segregio' },
     occupationTitle: {
       pt: 'Desenvolvedor Front-end',
       en: 'Front-end Developer',
@@ -24,10 +26,16 @@ export default function SectionMain() {
   };
 
   return (
-    <section>
-      <h3>{ language === 'pt-BR' ? languageObject.greetingTitle.pt : languageObject.greetingTitle.en }</h3>
-      <h3>{ languageObject.nameTitle.pt }</h3>
-      <h3>{ language === 'pt-BR' ? languageObject.occupationTitle.pt : languageObject.occupationTitle.en }</h3>
+    <section style={theme ? { backgroundColor: 'black', color: 'white' } : { backgroundColor: 'white', color: 'black' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', paddingLeft: '20%' }}>
+        <h3 style={{ fontSize: '40px', marginBottom: '2%' }}>{ language === 'pt-BR' ? languageObject.greetingTitle.pt : languageObject.greetingTitle.en }</h3>
+        <h3 style={{ fontSize: '115px', margin: '0' }}>{ languageObject.firstNameTitle.pt }</h3>
+        <h3 style={{ fontSize: '115px', margin: '0' }}>{ languageObject.lastNameTitle.pt }</h3>
+        <h3 style={{ fontSize: '30px' }}>{ language === 'pt-BR' ? languageObject.occupationTitle.pt : languageObject.occupationTitle.en }</h3>
+      </div>
+      <div>
+        <img src="" alt="" />
+      </div>
     </section>
   );
 }
