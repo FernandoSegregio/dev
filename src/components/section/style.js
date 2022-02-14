8c2c2f893c4e11636116b1c3e17bcb70d5e7366a
Fernando Segregio <segregio@gmail.com>
2022-02-14 20:57:48 -0300

    style + refactor: passa para o styled componente keyframe do title.

--- src/components/section/style.js
+++ src/components/section/style.js
@@ -23,6 +23,7 @@ export const Section = styled.section`
     margin-bottom: -50px;
     width: 580px;
     letter-spacing: -5px;
+    animation: changeTitleColor1 8s infinite;
   }
 
   .name-title2 {
@@ -30,11 +31,51 @@ export const Section = styled.section`
     font-weight: 800;
     letter-spacing: -5px;
     margin-bottom: 3%;
+    animation: changeTitleColor2 8s infinite;
   }
 
+  @keyframes changeTitleColor1 {
+    0% {
+      background-image: linear-gradient(to right, #007cf0, #00dfd8);
+      color: black;
+      background-clip: text;
+      -webkit-background-clip: text;
+
+      -webkit-text-fill-color: transparent;
+    }
+    70% {
+      color: #faebd7;
+    }
+
+    80% {
+      color: #ffffff;
+    }
+
+    100% {
+      color: white;
+    }
+  }
+
+  @keyframes changeTitleColor2 {
+    0% {
+      color: white;
+    }
+    20% {
+      color: antiquewhite;
+    }
+
+    100% {
+      background-image: linear-gradient(to right, #7928ca, #ff0080);
+      color: black;
+      background-clip: text;
+      -webkit-background-clip: text;
+
+      -webkit-text-fill-color: transparent;
+    }
+  }
 `;
 
 export const ImageMain = styled.img`
-   width: 265px; 
-   border-radius: 90px; 
+  width: 265px;
+  border-radius: 90px;
 `;
