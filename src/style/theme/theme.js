13123d06ef72351df2dd7e984589b7611e087797
Fernando Segregio <segregio@gmail.com>
2022-02-21 19:45:26 -0300

    style: adiciona novas cores aos temas dinamicos.

--- src/style/theme/theme.js
+++ src/style/theme/theme.js
@@ -4,12 +4,21 @@ export const light = {
   title70: '#302d2de8',
   title80: '#504c4ce8',
   title100: '#8c8787e8',
+  button: '#302d2de8',
+  textButton: '#504c4ce8',
+  backForms: '#fafafa',
+  border: '#504c4ce8',
 };
 
 export const dark = {
+
   background: '#000000e8',
   text: '#ffffff',
   title70: '#faebd7',
   title80: '#ffffff',
   title100: '#ffffff',
+  button: '#302d2de8',
+  textButton: '#faebd7',
+  backForms: '#504c4ce8',
+  border: '#000000',
 };
