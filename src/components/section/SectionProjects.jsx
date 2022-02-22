5afeaacbc7f95b65d86bc152337a632c5c148518
Fernando Segregio <segregio@gmail.com>
2022-02-22 17:55:55 -0300

    style: acerta dimensionamento do carrossel.

--- src/components/section/SectionProjects.jsx
+++ src/components/section/SectionProjects.jsx
@@ -10,20 +10,21 @@ export default function SectionProjects() {
   const { language, theme } = useContext(PortfolioContext);
   return (
     <ThemeProvider theme={theme === 'dark' ? dark : light}>
-      <SectionProjectsStyle className="carousel">
+      <SectionProjectsStyle>
         <h3 className="contact">
           {language === 'pt-BR'
             ? languageData.titleNav2.pt
             : languageData.titleNav2.en}
         </h3>
-
-        {projects.map((project) => (
-          <div key={project.name} className="card">
-            <img src={project.img} alt={project.name} />
-            <h4>{project.name}</h4>
-            <p>{project.desc}</p>
-          </div>
-        ))}
+        <div className="carousel">
+          {projects.map((project) => (
+            <div key={project.name} className="card">
+              <img src={project.img} alt={project.name} />
+              <h4>{project.name}</h4>
+              <p>{project.desc}</p>
+            </div>
+          ))}
+        </div>
       </SectionProjectsStyle>
     </ThemeProvider>
   );
