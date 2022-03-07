ba42754a8f5e231c7a5e45afc618f99ec0eb314c
Fernando Segregio <segregio@gmail.com>
2022-03-07 18:41:46 -0300

    feat: acrescenta ingles na descricao dos projetos.

--- src/components/section/SectionProjects.jsx
+++ src/components/section/SectionProjects.jsx
@@ -4,7 +4,7 @@ import { SectionProjectsStyle } from './style';
 import { dark, light } from '../../style/theme/theme';
 import PortfolioContext from '../../context/PortfolioContext';
 import languageData from '../../support/languageData';
-import projects from '../../support/projectsData';
+import { projects, projectsEng } from '../../support/projectsData';
 
 export default function SectionProjects() {
   const { language, theme } = useContext(PortfolioContext);
@@ -17,31 +17,57 @@ export default function SectionProjects() {
             : languageData.titleNav2.en}
         </h3>
         <div className="carousel">
-          {projects.map((project) => (
-            <div key={project.id} className="card">
-              <h4>
-                {project.name}
-                <span className="version">
-                  {project.mobile}
-                  {project.desktop}
-                </span>
-              </h4>
-              <img src={project.img} alt={project.name} />
-              <div>
-                <p className="desc-prj">{project.desc}</p>
-                <p className="title-skills">{project.skills}</p>
-                <div className="skills">
-                  { project.skills1 && <img src={project.skills1} alt="" />}
-                  { project.skills2 && <img src={project.skills2} alt="" />}
-                  { project.skills3 && <img src={project.skills3} alt="" />}
-                  { project.skills4 && <img src={project.skills4} alt="" />}
-                  { project.skills5 && <img src={project.skills5} alt="" />}
+          { language === 'pt-BR'
+            ? projects.map((project) => (
+              <div key={project.id} className="card">
+                <h4>
+                  {project.name}
+                  <span className="version">
+                    {project.mobile}
+                    {project.desktop}
+                  </span>
+                </h4>
+                <img src={project.img} alt={project.name} />
+                <div>
+                  <p className="desc-prj">{project.desc}</p>
+                  <p className="title-skills">{project.skills}</p>
+                  <div className="skills">
+                    { project.skills1 && <img src={project.skills1} alt="" />}
+                    { project.skills2 && <img src={project.skills2} alt="" />}
+                    { project.skills3 && <img src={project.skills3} alt="" />}
+                    { project.skills4 && <img src={project.skills4} alt="" />}
+                    { project.skills5 && <img src={project.skills5} alt="" />}
+                  </div>
+                  <a className="link-rep" href={project.repo}>{project.linkRep}</a>
+                  <a className="link-rep" href={project.link}>{project.name}</a>
                 </div>
-                <a className="link-rep" href={project.repo}>{project.linkRep}</a>
-                <a className="link-rep" href={project.link}>{project.name}</a>
               </div>
-            </div>
-          ))}
+            ))
+            : projectsEng.map((project) => (
+              <div key={project.id} className="card">
+                <h4>
+                  {project.name}
+                  <span className="version">
+                    {project.mobile}
+                    {project.desktop}
+                  </span>
+                </h4>
+                <img src={project.img} alt={project.name} />
+                <div>
+                  <p className="desc-prj">{project.desc}</p>
+                  <p className="title-skills">{project.skills}</p>
+                  <div className="skills">
+                    { project.skills1 && <img src={project.skills1} alt="" />}
+                    { project.skills2 && <img src={project.skills2} alt="" />}
+                    { project.skills3 && <img src={project.skills3} alt="" />}
+                    { project.skills4 && <img src={project.skills4} alt="" />}
+                    { project.skills5 && <img src={project.skills5} alt="" />}
+                  </div>
+                  <a className="link-rep" href={project.repo}>{project.linkRep}</a>
+                  <a className="link-rep" href={project.link}>{project.name}</a>
+                </div>
+              </div>
+            ))}
         </div>
       </SectionProjectsStyle>
     </ThemeProvider>
